8d3a996926dbb9686d31b58cee2eb37b003eb7a1
Zeyad Mohamed <zeyadmohamedasu@gmail.com>
2023-12-19 07:02:25 +0200

    Add the rest grammer rules functions

--- Parser Project/parser.cpp
+++ Parser Project/parser.cpp
@@ -27,7 +27,7 @@ SyntaxTree* Parser::stmt_sequence()
 /* Rule: statement → if-stmt | repeat-stmt | assign-stmt | read-stmt | write-stmt */
 SyntaxTree* Parser::statement()
 {
-    SyntaxTree* statementTree;
+    SyntaxTree* statementTree = nullptr;
     /* check the possible first tokens of each type of statement rule */
     switch(tokens[index]->type)
     {
@@ -84,7 +84,7 @@ SyntaxTree* Parser::if_stmt()
 SyntaxTree* Parser::repeat_stmt()
 {
     SyntaxTree* repeatStmtTree = new SyntaxTree();
-    repeatStmtTree->add_node_data(READ_STATEMENT, "REPEAT");
+    repeatStmtTree->add_node_data(REPEAT_STATEMENT, "REPEAT");
     match(REPEAT);
     repeatStmtTree->add_child(stmt_sequence());     /* Body child */
     match(UNTIL);
@@ -96,12 +96,12 @@ SyntaxTree* Parser::repeat_stmt()
 /* Rule: assign-stmt → identifier := exp */
 SyntaxTree* Parser::assign_stmt()
 {
-    SyntaxTree* expTree = new SyntaxTree();
-    expTree->add_node_data(ASSIGN_STATEMENT, ":=");
+    SyntaxTree* assignStmtTree = new SyntaxTree();
+    assignStmtTree->add_node_data(ASSIGN_STATEMENT, ":=");
     match(IDENTIFIER);
     match(ASSIGN);
-    expTree->add_child(exp());              /* Exp child */
-    return expTree;
+    assignStmtTree->add_child(exp());              /* Exp child */
+    return assignStmtTree;
 }
 
 /* Rule: read-stmt → read identifier */
@@ -114,7 +114,156 @@ SyntaxTree* Parser::read_stmt()
     return readStmtTree;
 }
 
+/* Rule: write-stmt → write exp */
+SyntaxTree* Parser::write_stmt()
+{
+    SyntaxTree* writeStmtTree = new SyntaxTree();
+    match(WRITE);
+    writeStmtTree->add_node_data(WRITE_STATEMENT, "WRITE");
+    writeStmtTree->add_child(exp());
+    return writeStmtTree;
+}
 
+/* Rule: exp → simple-exp comparison-op simple-exp | simple-exp
+   EBNF: simple-exp [comparison-op simple-exp]
+*/
+SyntaxTree* Parser::exp()
+{
+    SyntaxTree* expStmtTree = simple_exp();
+    if (LESSTHAN == tokens[index]->type || EQUAL == tokens[index]->type)
+    {
+        SyntaxTree* comparisonOpStmtTree = comparison_op();
+        comparisonOpStmtTree->add_child(expStmtTree);
+        comparisonOpStmtTree->add_child(simple_exp());
+        expStmtTree = comparisonOpStmtTree;
+    }
+    return expStmtTree;
+}
+
+/* Rule: comparison-op → < | = */
+SyntaxTree* Parser::comparison_op()
+{
+    SyntaxTree* comparisonOpStmtTree = new SyntaxTree();
+    if (LESSTHAN == tokens[index]->type)
+    {
+        match(LESSTHAN);
+        comparisonOpStmtTree->add_node_data(OPERATOR_EXPRESSION, "<");
+    }
+    else if (EQUAL == tokens[index]->type)
+    {
+        match(EQUAL);
+        comparisonOpStmtTree->add_node_data(OPERATOR_EXPRESSION, "=");
+    }
+    else
+    {
+        errors.insert(index);
+    }
+    return comparisonOpStmtTree;
+}
+
+/* Rule: simple-exp → simple-exp addop term | term
+   EBNF: term {addop term}
+*/
+SyntaxTree* Parser::simple_exp()
+{
+    SyntaxTree* simpleExpTree = term();
+    while (PLUS == tokens[index]->type || MINUS == tokens[index]->type)
+    {
+        SyntaxTree* addOpTree =  addop();
+        addOpTree->add_child(simpleExpTree);
+        addOpTree->add_child(term());
+        simpleExpTree = addOpTree;
+    }
+    return simpleExpTree;
+}
+
+/* Rule: addop → + | - */
+SyntaxTree* Parser::addop()
+{
+    SyntaxTree* addOpTree = new SyntaxTree();
+    if (PLUS == tokens[index]->type)
+    {
+        match(PLUS);
+        addOpTree->add_node_data(OPERATOR_EXPRESSION, "+");
+    }
+    else if (MINUS == tokens[index]->type)
+    {
+        match(MINUS);
+        addOpTree->add_node_data(OPERATOR_EXPRESSION, "-");
+    }
+    else
+    {
+        errors.insert(index);
+    }
+    return addOpTree;
+}
+
+/* Rule: term → term mulop factor | factor
+   EBNF: factor {mulop factor}
+*/
+SyntaxTree* Parser::term()
+{
+    SyntaxTree* termTree = factor();
+    while (MULT == tokens[index]->type || DIV == tokens[index]->type)
+    {
+        SyntaxTree* mulOpTree = mulop();
+        mulOpTree->add_child(termTree);
+        mulOpTree->add_child(factor());
+        termTree = mulOpTree;
+    }
+    return termTree;
+}
+
+/* Rule: mulop → * | / */
+SyntaxTree* Parser::mulop()
+{
+    SyntaxTree* mulOpTree = new SyntaxTree();
+    if (MULT == tokens[index]->type)
+    {
+        match(MULT);
+        mulOpTree->add_node_data(OPERATOR_EXPRESSION, "*");
+    }
+    else if (DIV == tokens[index]->type)
+    {
+        match(DIV);
+        mulOpTree->add_node_data(OPERATOR_EXPRESSION, "/");
+    }
+    else
+    {
+        errors.insert(index);
+    }
+    return mulOpTree;
+}
+
+/* Rule: factor → (exp) | number | identifier */
+SyntaxTree* Parser::factor()
+{
+    SyntaxTree* factorTree = nullptr;
+    if (OPENBRACKET == tokens[index]->type)
+    {
+        match(OPENBRACKET);
+        factorTree = exp();
+        factorTree->add_node_data(NO_EXPRESSION, tokens[index]->value);
+        match(CLOSEDBRACKET);
+    }
+    else if (NUMBER == tokens[index]->type)
+    {
+        match(NUMBER);
+        factorTree = new SyntaxTree();
+        factorTree->add_node_data(NO_EXPRESSION, tokens[index]->value);
+    }
+    else if (IDENTIFIER == tokens[index]->type)
+    {
+        match(IDENTIFIER);
+        factorTree = new SyntaxTree();
+        factorTree->add_node_data(NO_EXPRESSION, tokens[index]->value);
+    }
+    else
+    {
+        errors.insert(index);
+    }
+    return factorTree;
+}
 
 /* Update the index of the token */
 void Parser::match(TokenType tokenType)
